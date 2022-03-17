89b0fcbd409361b8983359783027c0ee7850e798
Mays Jones <maysjones2@gmail.com>
2022-03-17 11:00:19 -0400

    commit tree 11e7207ecadac0fd4b9e35da7c6a0ec2deedfab7

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+  let result;
+  if ( ride <= 400){
+    result = 'This one is on me!'
+  }
+  else if ( ride > 2500){
+    result = 'No can do.'
+  }
+  else if ( ride > 2000){
+    result = 'I will gladly take your thirty bucks.'
+  }
+  return result;
 }
+console.log (scuberGreetingForFeet(2001))
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function ternaryCheckCity(city){
+const canIGo = city === 'NYC' ? 'Ok, sounds good.': 'No go.'
+return canIGo;} 
+console.log (ternaryCheckCity('NYC'))
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(tip){
+  let charm;
+  switch (tip){
+case 'generous' : charm = "Thank you so much."
+  break;
+case 'not as generous' : charm = "Thank you."
+  break;
+default : charm = "Bye."} return charm;}
